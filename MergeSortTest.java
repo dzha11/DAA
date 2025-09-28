97332245e81a290c4d92b6083b0a88d582c0415a
dzha11 <241321@astanait.edu.kz>
2025-09-28 23:11:45 +0500

    Update MergeSortTest.java

--- MergeSortTest.java
+++ MergeSortTest.java
@@ -2,12 +2,36 @@ package algorithms;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.Random;
+import java.util.Arrays;
 
 public class MergeSortTest {
+
     @Test
-    void testMergeSort() {
-        int[] arr = {5, 2, 9, 1, 3};
+    void simpleSort() {
+        int[] arr = {9, 7, 5, 3, 1};
         MergeSort.sort(arr);
-        assertArrayEquals(new int[]{1, 2, 3, 5, 9}, arr);
+        assertArrayEquals(new int[]{1,3,5,7,9}, arr);
+    }
+
+    @Test
+    void randomLots() {
+        Random rnd = new Random(123);
+        for (int t = 0; t < 50; t++) {
+            int n = 100;
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = rnd.nextInt(500);
+            int[] expected = a.clone();
+            Arrays.sort(expected);
+            MergeSort.sort(a);
+            assertArrayEquals(expected, a);
+        }
+    }
+
+    @Test
+    void duplicatesAndSmall() {
+        int[] arr = {2,2,2,1,1,3,2};
+        MergeSort.sort(arr);
+        assertArrayEquals(new int[]{1,1,2,2,2,2,3}, arr);
     }
 }
