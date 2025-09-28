fb793d72f5aad13ef427ac6d20be369983997d7c
dzha11 <241321@astanait.edu.kz>
2025-09-28 23:11:17 +0500

    Update DeterministicSelectTest.java

--- DeterministicSelectTest.java
+++ DeterministicSelectTest.java
@@ -2,12 +2,31 @@ package algorithms;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.Random;
+import java.util.Arrays;
 
 public class DeterministicSelectTest {
+
     @Test
-    void testSelect() {
-        int[] arr = {10, 4, 5, 8, 6, 11, 26};
-        int result = DeterministicSelect.select(arr, 3);
-        assertEquals(8, result);
+    void smallExample() {
+        int[] a = {7,2,9,1,5};
+        int res = DeterministicSelect.select(a.clone(), 2); // 3rd smallest (0-based)
+        assertEquals(5, res);
+    }
+
+    @Test
+    void compareWithSortManyRandoms() {
+        Random rnd = new Random(42);
+        for (int t = 0; t < 100; t++) {
+            int n = 100;
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = rnd.nextInt(1000);
+            int k = rnd.nextInt(n);
+            int[] sorted = a.clone();
+            Arrays.sort(sorted);
+            int expected = sorted[k];
+            int got = DeterministicSelect.select(a.clone(), k);
+            assertEquals(expected, got);
+        }
     }
 }
