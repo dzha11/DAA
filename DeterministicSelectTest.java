b6c9c0e03eb105394bdb3b92046525d09b4a3820
dzha11 <241321@astanait.edu.kz>
2025-09-28 22:51:38 +0500

    Update DeterministicSelectTest.java

--- DeterministicSelectTest.java
+++ DeterministicSelectTest.java
@@ -4,11 +4,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DeterministicSelectTest {
-
     @Test
     void testSelect() {
-        int[] arr = {7, 2, 9, 1, 5};
-        int result = DeterministicSelect.select(arr, 2); // третий по возрастанию
-        assertEquals(5, result);
+        int[] arr = {10, 4, 5, 8, 6, 11, 26};
+        int result = DeterministicSelect.select(arr, 3);
+        assertEquals(8, result);
     }
 }
