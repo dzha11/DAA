77613159d6ab81d21b75b1d4a66ca14d063465df
dzha11 <241321@astanait.edu.kz>
2025-09-28 23:12:12 +0500

    Update QuickSortTest.java

--- QuickSortTest.java
+++ QuickSortTest.java
@@ -2,12 +2,38 @@ package algorithms;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.Random;
+import java.util.Arrays;
 
 public class QuickSortTest {
+
     @Test
-    void testQuickSort() {
-        int[] arr = {7, 4, 1, 9, 2};
+    void simpleSort() {
+        int[] arr = {7, 4, 6, 2, 8, 1};
         QuickSort.sort(arr);
-        assertArrayEquals(new int[]{1, 2, 4, 7, 9}, arr);
+        assertArrayEquals(new int[]{1,2,4,6,7,8}, arr);
+    }
+
+    @Test
+    void randomVsArraysSort() {
+        Random rnd = new Random(99);
+        for (int t = 0; t < 50; t++) {
+            int n = 150;
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = rnd.nextInt(1000) - 500;
+            int[] exp = a.clone(); Arrays.sort(exp);
+            QuickSort.sort(a);
+            assertArrayEquals(exp, a);
+        }
+    }
+
+    @Test
+    void adversarialSortedInput() {
+        int n = 200;
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = i; // already sorted
+        QuickSort.sort(a); // should not blow stack or fail
+        int[] exp = a.clone(); Arrays.sort(exp);
+        assertArrayEquals(exp, a);
     }
 }
