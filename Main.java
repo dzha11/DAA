587c38a6cdf4c7f05f11f87ca6d1e4fa5f6a4389
dzha11 <241321@astanait.edu.kz>
2025-09-28 22:48:50 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,32 +1,31 @@
 package algorithms;
+
 import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
-        int[] arr1 = {5, 2, 9, 1, 6};
-        System.out.println("Before MergeSort: " + Arrays.toString(arr1));
+        int[] arr1 = {5, 2, 9, 1, 3};
         MergeSort.sort(arr1);
-        System.out.println("After MergeSort: " + Arrays.toString(arr1));
+        System.out.println("MergeSort: " + Arrays.toString(arr1));
 
-        int[] arr2 = {10, 7, 8, 9, 1, 5};
-        System.out.println("\nBefore QuickSort: " + Arrays.toString(arr2));
+        int[] arr2 = {7, 4, 1, 9, 2};
         QuickSort.sort(arr2);
-        System.out.println("After QuickSort: " + Arrays.toString(arr2));
+        System.out.println("QuickSort: " + Arrays.toString(arr2));
 
-        int[] arr3 = {12, 3, 5, 7, 4, 19, 26};
-        int k = 2;
-        int kthSmallest = DeterministicSelect.select(arr3, k);
-        System.out.println("\n" + k + "-th smallest element is: " + kthSmallest);
+        int[] arr3 = {10, 4, 5, 8, 6, 11, 26};
+        int k = 3;
+        int kth = DeterministicSelect.select(arr3, k);
+        System.out.println("k=" + k + " element = " + kth);
 
-        ClosestPair.Point[] points = {
-                new ClosestPair.Point(2, 3),
-                new ClosestPair.Point(12, 30),
-                new ClosestPair.Point(40, 50),
-                new ClosestPair.Point(5, 1),
-                new ClosestPair.Point(12, 10),
-                new ClosestPair.Point(3, 4)
+        Point[] points = {
+                new Point(2, 3),
+                new Point(12, 30),
+                new Point(40, 50),
+                new Point(5, 1),
+                new Point(12, 10),
+                new Point(3, 4)
         };
-        double minDist = ClosestPair.findClosestPair(points);
-        System.out.println("\nThe smallest distance is: " + minDist);
+        double minDist = ClosestPair.closest(points);
+        System.out.println("Closest Pair distance: " + minDist);
     }
 }
