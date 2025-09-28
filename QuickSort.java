a5ce0afec43ef3a24c98213a5c418c79106991cf
dzha11 <241321@astanait.edu.kz>
2025-09-28 22:53:09 +0500

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -1,20 +1,34 @@
 package algorithms;
+
+import java.util.Random;
+
 public class QuickSort {
+
+    private static final Random rand = new Random();
+
     public static void sort(int[] arr) {
         if (arr == null || arr.length < 2) return;
         quickSort(arr, 0, arr.length - 1);
     }
 
     private static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
+        while (low < high) {
             int pivotIndex = partition(arr, low, high);
-            quickSort(arr, low, pivotIndex - 1);
-            quickSort(arr, pivotIndex + 1, high);
+            if (pivotIndex - low < high - pivotIndex) {
+                quickSort(arr, low, pivotIndex - 1);
+                low = pivotIndex + 1;
+            } else {
+                quickSort(arr, pivotIndex + 1, high);
+                high = pivotIndex - 1;
+            }
         }
     }
 
     private static int partition(int[] arr, int low, int high) {
-        int pivot = arr[high]; // берём последний элемент как опорный
+        int pivotIndex = low + rand.nextInt(high - low + 1);
+        swap(arr, pivotIndex, high);
+
+        int pivot = arr[high];
         int i = low - 1;
         for (int j = low; j < high; j++) {
             if (arr[j] <= pivot) {
@@ -27,8 +41,8 @@ public class QuickSort {
     }
 
     private static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
+        int tmp = arr[i];
         arr[i] = arr[j];
-        arr[j] = temp;
+        arr[j] = tmp;
     }
 }
