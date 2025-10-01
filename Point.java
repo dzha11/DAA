10ade24e154ae1064af4b80118a81d056479ef79
dzha11 <241321@astanait.edu.kz>
2025-10-01 14:35:26 +0500

    Point.java

--- Point.java
+++ Point.java
@@ -2,9 +2,5 @@ package algorithms;
 
 public class Point {
     public double x, y;
-
-    public Point(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
+    public Point(double x, double y) { this.x = x; this.y = y; }
 }
