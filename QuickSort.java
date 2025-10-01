5df31b918f4f533b1dcf97aed744c34555dde820
dzha11 <241321@astanait.edu.kz>
2025-10-01 14:35:53 +0500

    QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -1,48 +1,31 @@
 package algorithms;
 
-import java.util.Random;
-
 public class QuickSort {
-
-    private static final Random rand = new Random();
-
-    public static void sort(int[] arr) {
-        if (arr == null || arr.length < 2) return;
-        quickSort(arr, 0, arr.length - 1);
+    public static void sort(int[] arr, Metrics m) {
+        quickSort(arr, 0, arr.length - 1, m);
     }
 
-    private static void quickSort(int[] arr, int low, int high) {
-        while (low < high) {
-            int pivotIndex = partition(arr, low, high);
-            if (pivotIndex - low < high - pivotIndex) {
-                quickSort(arr, low, pivotIndex - 1);
-                low = pivotIndex + 1;
-            } else {
-                quickSort(arr, pivotIndex + 1, high);
-                high = pivotIndex - 1;
-            }
+    private static void quickSort(int[] arr, int low, int high, Metrics m) {
+        if (low < high) {
+            m.enterRecursion();
+            int pi = partition(arr, low, high, m);
+            quickSort(arr, low, pi - 1, m);
+            quickSort(arr, pi + 1, high, m);
+            m.exitRecursion();
         }
     }
 
-    private static int partition(int[] arr, int low, int high) {
-        int pivotIndex = low + rand.nextInt(high - low + 1);
-        swap(arr, pivotIndex, high);
-
+    private static int partition(int[] arr, int low, int high, Metrics m) {
         int pivot = arr[high];
         int i = low - 1;
         for (int j = low; j < high; j++) {
+            m.incComparisons();
             if (arr[j] <= pivot) {
                 i++;
-                swap(arr, i, j);
+                int tmp = arr[i]; arr[i] = arr[j]; arr[j] = tmp;
             }
         }
-        swap(arr, i + 1, high);
+        int tmp = arr[i + 1]; arr[i + 1] = arr[high]; arr[high] = tmp;
         return i + 1;
-    }
-
-    private static void swap(int[] arr, int i, int j) {
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
     }
 }
